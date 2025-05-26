9ee951ced88b22a441124fb05f52479bf405d91a
Shouryan Nikam <dev.shouryan@gmail.com>
2025-05-26 11:46:29 -0700

    comment out footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,7 +111,7 @@ export default function RootLayout({
                       {children}
                     </div>
                   </main>
-                  <Footer />
+                  {/* <Footer /> */}
                 </div>
                 <CommandK />
               </SwrConfigClient>
